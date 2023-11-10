3472074fd65e022a6047cca55af182b05a25461a
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-11-10 14:59:04 +0300

    cloning the item

--- src/stores/CartStore.ts
+++ src/stores/CartStore.ts
@@ -12,7 +12,7 @@ export const useCartStore = defineStore("CartStore", {
     addItems(count, item) {
       count = parseInt(count);
       for (let index = 0; index < count; index++) {
-        this.items.push(item);
+        this.items.push({ ...item });
       }
     },
   },
