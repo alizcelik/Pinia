d985565c0806e6169c7ee7f779e3f6e473ac3c4d
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-11-10 17:39:20 +0300

    OptionsAPI - mapState and mapWritableState

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,32 @@
-<script setup>
+<script>
 // imports
-import CartWidget from './CartWidget.vue';
+import { useAuthUserStore } from "../stores/AuthUserStore";
+import { mapWritableState } from "pinia";
+import CartWidget from "./CartWidget.vue";
+
+export default {
+  components: {
+    CartWidget,
+  },
+
+  computed: {
+    ...mapWritableState(useAuthUserStore, {
+      user: "username",
+    }),
+  },
+};
 </script>
 
 <template>
-  <header 
+  <header
     class="flex justify-between p-6 mb-10 items-center"
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div>
+      <input v-model="user" type="text" />
+      <span class="mr-5">{{ user }}</span>
+      <CartWidget class="inline-block" />
+    </div>
   </header>
 </template>
