3f5dc82b06670aafab11bfb05d01f7b5411167df
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-11-13 11:47:03 +0300

    Subscribe to the State and undo

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,29 @@ import ProductCard from "@/components/ProductCard.vue";
 import { useCartStore } from "@/stores/CartStore";
 
 import { useProductStore } from "@/stores/ProductStore";
+import { ref, reactive } from "vue";
+import AppButton from "@/components/AppButton.vue";
 //import { storeToRefs } from "pinia";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
+
+const history = reactive([]);
+const doingHistory = ref(false);
+history.push(JSON.stringify(cartStore.$state));
+const undo = () => {
+  if (history.length === 1) return;
+  doingHistory.value = true;
+  history.pop();
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+};
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state));
+  }
+});
+
 //const { products } = storeToRefs(useProductStore());
 cartStore.$onAction(({ name, store, args, after, onError }) => {
   if (name === "addItems") {
@@ -26,6 +45,9 @@ productStore.fill();
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
